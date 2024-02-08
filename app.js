6329c6ad2d2ff16d049b6e2f406d5ccac3fcefb9
Luis Valladolid <20213tn002@utez.edu.mx>
2024-02-08 13:44:55 -0600

    Create Mascotas

--- app.js
+++ app.js
@@ -22,3 +22,42 @@ const createClient = () => {
 }
 
 setInterval(createClient,5000);
+
+const createPets = () => {
+    const petsName = ["Juan", "Pulgoso", "Manchas", "Matias", "Zacarias", "Pulgas", "Pedro", "Alfonso", "Manuel", "Alejandro"];
+    const animalTypes = ["Dog", "Cat", "Bird", "Fish", "Hamster", "Turtle", "Rabbit", "Guinea Pig", "Snake", "Lizard"];
+    
+    const randomPetName = petsName[Math.floor(Math.random() * petsName.length)];
+    const randomAnimalType = animalTypes[Math.floor(Math.random() * animalTypes.length)];
+    
+    let randomBreed;
+
+    switch (randomAnimalType) {
+        case "Dog":
+            const dogBreeds = ["San Bernardo", "Labrador", "Poodle", "Bulldog", "Chihuahua"];
+            randomBreed = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+            break;
+        case "Cat":
+            const catBreeds = ["Siamese", "Persian", "Maine Coon", "Bengal", "Sphynx"];
+            randomBreed = catBreeds[Math.floor(Math.random() * catBreeds.length)];
+            break;
+        default:
+            randomBreed = "Unknown";
+    }
+
+    // Consulta para obtener un cliente aleatorio de la base de datos
+    const clientSql = "SELECT id FROM clients ORDER BY RAND() LIMIT 1";
+    db.query(clientSql, (err, clientResult) => {
+        if (err) throw err;
+        const owner_id = clientResult[0].id;
+        
+        const sql = `INSERT INTO pets (name, type, breed, owner_id, created_at) VALUES ('${randomPetName}', '${randomAnimalType}', '${randomBreed}', '${owner_id}', '2024-02-01 19:07:41')`;
+
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log('Mascota creada!');
+        });
+    });
+}
+
+setInterval(createPets, 4000);
--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: 'localhost',
     user: 'root',
-    password: 'admin',
+    password: 'root',
     database: 'vet',
     port: '3306',
 });
